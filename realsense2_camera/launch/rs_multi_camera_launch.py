713b9e7fe8714fee73010647d5603e188b7e84f6
Louis-Gabriel Pouillot <lgp@hibox.fr>
2023-02-15 18:10:14 +0100

    add 3 cameras to rs_multi_camera default launch script

--- realsense2_camera/launch/rs_multi_camera_launch.py
+++ realsense2_camera/launch/rs_multi_camera_launch.py
@@ -1,16 +1,6 @@
-# Copyright 2023 Intel Corporation. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# License: Apache 2.0. See LICENSE file in root directory.
+# Copyright(c) 2022 Intel Corporation. All Rights Reserved.
+
 
 # DESCRIPTION #
 # ----------- #
@@ -30,15 +20,22 @@ from launch.substitutions import LaunchConfiguration, ThisLaunchFileDir
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 import sys
 import pathlib
+
 sys.path.append(str(pathlib.Path(__file__).parent.absolute()))
 import rs_launch
 
+# print(sys.path)
+# print(dir(rs_launch))
+# sys.exit(0)
 local_parameters = [{'name': 'camera_name1', 'default': 'camera1', 'description': 'camera unique name'},
                     {'name': 'camera_name2', 'default': 'camera2', 'description': 'camera unique name'},
-                   ]
+                    {'name': 'camera_name3', 'default': 'camera3', 'description': 'camera unique name'},
+                    ]
+
 
 def set_configurable_parameters(local_params):
     return dict([(param['original_name'], LaunchConfiguration(param['name'])) for param in local_params])
+
 
 def duplicate_params(general_params, posix):
     local_params = copy.deepcopy(general_params)
@@ -46,28 +43,39 @@ def duplicate_params(general_params, posix):
         param['original_name'] = param['name']
         param['name'] += posix
     return local_params
-    
+
 
 def generate_launch_description():
     params1 = duplicate_params(rs_launch.configurable_parameters, '1')
     params2 = duplicate_params(rs_launch.configurable_parameters, '2')
+    params3 = duplicate_params(rs_launch.configurable_parameters, '3')
     return LaunchDescription(
         rs_launch.declare_configurable_parameters(local_parameters) +
-        rs_launch.declare_configurable_parameters(params1) + 
-        rs_launch.declare_configurable_parameters(params2) + 
+        rs_launch.declare_configurable_parameters(params1) +
+        rs_launch.declare_configurable_parameters(params2) +
+        rs_launch.declare_configurable_parameters(params3) +
         [
-        IncludeLaunchDescription(
-            PythonLaunchDescriptionSource([ThisLaunchFileDir(), '/rs_launch.py']),
-            launch_arguments=set_configurable_parameters(params1).items(),
-        ),
-        IncludeLaunchDescription(
-            PythonLaunchDescriptionSource([ThisLaunchFileDir(), '/rs_launch.py']),
-            launch_arguments=set_configurable_parameters(params2).items(),
-        ),
-        # dummy static transformation from camera1 to camera2
-        launch_ros.actions.Node(
-            package = "tf2_ros",
-            executable = "static_transform_publisher",
-            arguments = ["0", "0", "0", "0", "0", "0", "camera1_link", "camera2_link"]
-        ),
-    ])
+            IncludeLaunchDescription(
+                PythonLaunchDescriptionSource([ThisLaunchFileDir(), '/rs_launch.py']),
+                launch_arguments=set_configurable_parameters(params1).items(),
+            ),
+            IncludeLaunchDescription(
+                PythonLaunchDescriptionSource([ThisLaunchFileDir(), '/rs_launch.py']),
+                launch_arguments=set_configurable_parameters(params2).items(),
+            ),
+            IncludeLaunchDescription(
+                PythonLaunchDescriptionSource([ThisLaunchFileDir(), '/rs_launch.py']),
+                launch_arguments=set_configurable_parameters(params3).items(),
+            ),
+            # dummy static transformation from camera1 to camera2
+            launch_ros.actions.Node(
+                package="tf2_ros",
+                executable="static_transform_publisher",
+                arguments=["0", "0", "0", "0", "0", "0", "camera1_link", "camera2_link"]
+            ),
+            launch_ros.actions.Node(
+                package="tf2_ros",
+                executable="static_transform_publisher",
+                arguments=["0", "0", "0", "0", "0", "0", "camera2_link", "camera3_link"]
+            ),
+        ])
